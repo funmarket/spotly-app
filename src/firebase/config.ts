d867075c0810f7a69a878f79619ec791a26ebfee
FUN MARKET <funmarketmn@gmail.com>
2025-11-21 16:08:25 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8433025498-13bb2",
+  "appId": "1:126845855020:web:df395f55c51aab6174f889",
+  "apiKey": "AIzaSyD8y36I5vF7WZ8lY1mkJkxOCznXA1SwgK8",
+  "authDomain": "studio-8433025498-13bb2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "126845855020"
+};
